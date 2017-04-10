f10f933eab25dde2bf3e2052030479934cad8efb
Iryna Grynchak <irina_dreshpan@list.ru>
2017-04-11 00:19:29 +0300

    Project2

---
+++ home-build/src/js/main.js
@@ -0,0 +1,15 @@
+//= ../../node_modules/jquery-3.1.1.min.js
+
+/*
+Custom
+ */
+//= partials/jquery.ui.widget.js
+//= partials/jquery.event.drag.js
+//= partials/jquery.rs.carousel.js
+//= partials/jquery.rs.carousel-autoscroll.js
+//= partials/jquery.rs.carousel-continuous.js
+//= partials/jquery.rs.carousel-touch.js
+
+//= partials/slider.js
+
+//= partials/accordion.js
