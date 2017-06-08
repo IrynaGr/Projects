64cbfa9c61fea7e355ccd61423f860a35724adec
Iryna Grynchak <irina_dreshpan@list.ru>
2017-06-08 22:35:32 +0300

    Portfolio2

--- Portfolio-site/application.php
+++ Portfolio-site/application.php
@@ -3,37 +3,31 @@
 <head>
 <meta charset="UTF-8">
 <title>Создание формы обратной связи</title>
-<meta http-equiv="Refresh" content="4; URL=http://index.html/"> 
+<meta http-equiv="Refresh" content="4; URL=http://irynagr.zzz.com.ua/index.html"> 
 </head>
 <body>
 
 <?php 
 
-$sendto   = "ir.grynchak@gmail.com"; // почта, на которую будет приходить письмо
-$username = $_POST['name'];   // сохраняем в переменную данные полученные из поля c именем
-$usertel = $_POST['help']; // сохраняем в переменную данные полученные из поля c телефонным номером
-$usermail = $_POST['email']; // сохраняем в переменную данные полученные из поля c адресом электронной почты
-
-// Формирование заголовка письма
-$subject  = "Новое сообщение";
-$headers  = "From: " . strip_tags($usermail) . "\r\n";
-$headers .= "Reply-To: ". strip_tags($usermail) . "\r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html;charset=utf-8 \r\n";
-
-// Формирование тела письма
-$msg  = "<html><body style='font-family:Arial,sans-serif;'>";
-$msg .= "<h2 style='font-weight:bold;border-bottom:1px dotted #ccc;'>Cообщение с сайта</h2>\r\n";
-$msg .= "<p><strong>От кого:</strong> ".$username."</p>\r\n";
-$msg .= "<p><strong>Почта:</strong> ".$usermail."</p>\r\n";
-$msg .= "<p><strong>Телефон:</strong> ".$usertel."</p>\r\n";
-$msg .= "</body></html>";
-
-// отправка сообщения
-if(@mail($sendto, $subject, $msg, $headers)) {
-	echo "<center><img src='img/sent.png'></center>";
-} else {
-	echo "<center><img src='img/no-sent.png'></center>";
+if($_POST)
+{
+$to = "ir.grynchak@gmail.com";
+$from = 'very@irynagr.zzz.com.ua';
+$subject = "Ваше сообщение"; 
+ 
+$message = 'Имя: '.$_POST['name'].'; help: '.$_POST['help'].'; Email: '.$_POST['email'].';';
+ 
+$headers  = "Content-type: text/html; charset=UTF-8 \r\n"; 
+$headers .= "From: <very@irynagr.zzz.com.ua>\r\n"; 
+ 
+$result = mail($to, $subject, $message, $headers);
+ 
+ 
+if ($result){
+    echo "<center><img src='img/sent.png'></center>";
+    }
+else {echo "<center><img src='img/no-sent.png'></center>";
+    }
 }
 
 ?>
